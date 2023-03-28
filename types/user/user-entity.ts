00a5c51a1367bc4d1d0d3a661d24390e09be785a
Adam Watchmaker <watchmaker1337@gmail.com>
2023-03-28 17:18:45 +0200

    add user interface

---
+++ types/user/user-entity.ts
@@ -0,0 +1,12 @@
+export interface UserEntity{
+    id?:string;
+    username:string;
+    password:string;
+    email:string;
+    allExp:number;
+    workoutExp:number;
+    learningExp:number;
+    nutritionExp:number;
+    recreationExp:number;
+    hobbyExp:number;
+}
