7c028517f150c0e1aed7d6b1deed6fa014603a39
Adam Watchmaker <watchmaker1337@gmail.com>
2023-03-29 13:38:14 +0200

    tests user-record

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import { User } from './records/user-record';
 
 const app = express();
 
 app.use(cors({
 	origin: 'http://localhost:3000',
 }));
+
+// eslint-disable-next-line no-unused-vars
+app.get('/', async (req, res) => {
+	const user = await User.getOne('bc47f314-ce24-11ed-b79e-2cf05d21c3a3');
+	console.log(user);
+});
 
 app.use(express.json());
 
