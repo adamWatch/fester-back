324d1b3634471ae7eed05c123be9e4b9be928f9e
Adam Watchmaker <watchmaker1337@gmail.com>
2023-03-29 13:37:53 +0200

    add user-record

---
+++ records/user-record.ts
@@ -0,0 +1,54 @@
+import { FieldPacket } from 'mysql2';
+// import { v4 as uuid } from 'uuid';
+import { pool } from '../utils/db';
+import { UserEntity } from '../types/user';
+import { ValidationError } from '../utils/error';
+
+type UserResults = [UserEntity[], FieldPacket[]]
+
+export class User implements UserEntity {
+	public id?: string;
+
+	public username: string;
+
+	public email: string;
+
+	public password: string;
+
+	public allExp: number;
+
+	public workoutExp: number;
+
+	public learningExp: number;
+
+	public nutritionExp: number;
+
+	public recreationExp: number;
+
+	public hobbyExp: number;
+
+	constructor(obj:any) {
+		if (!obj.username || obj.length > 34) {
+			throw new ValidationError('Username not exist or too long max length is 34');
+		}
+
+		this.id = obj.id;
+		this.username = obj.username;
+		this.email = obj.email;
+		this.password = obj.password;
+		this.allExp = obj.allExp;
+		this.workoutExp = obj.workoutExp;
+		this.learningExp = obj.learningExp;
+		this.nutritionExp = obj.nutritionExp;
+		this.recreationExp = obj.recreationExp;
+		this.hobbyExp = obj.hobbyExp;
+	}
+
+	static async getOne(id:string):Promise<UserEntity |null> {
+		const [results,] = await pool.execute('SELECT * FROM `users` WHERE id = :id', {
+			id,
+		}) as UserResults;
+
+		return results.length === 0 ? null : new User(results[0]);
+	}
+}
