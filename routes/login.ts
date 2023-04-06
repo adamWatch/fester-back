b486c586eda082e6db064ef31371a768fc0b2c8d
Adam Watchmaker <watchmaker1337@gmail.com>
2023-04-06 17:27:04 +0200

    update login routes

--- routes/login.ts
+++ routes/login.ts
@@ -1,29 +1,44 @@
 import { Router } from 'express';
 import { User } from '../records/user-record';
-import { Notice } from '../types';
+import { Notice, NoticeWithId } from '../types';
+import { UserLogged } from '../records/user-logged-record';
 
 export const LoginRouter = Router()
 
+	// eslint-disable-next-line consistent-return
 	.post('/login', async (req, res) => {
 		const { body, } = req;
 		const user = await User.getOneByEmail(body.email);
 		console.log(user);
-		if (user.password === body.password) {
-			const notice:Notice = {
-				isVisible: true,
-				noticeText: 'You are log in',
-				noticeColor: 'green',
-			};
-
-			res.json(notice);
-		}
-		if (user.password !== body.password) {
+		if (user === null) {
 			const notice:Notice = {
 				isVisible: true,
 				noticeText: 'You email or password doesn\'t exist',
 				noticeColor: 'red',
 			};
+			return res.json(notice);
+		}
+		if (user !== null) {
+			if (user.password === body.password) {
+				const userIsLogged = new UserLogged(user.id, user.username);
+				userIsLogged.insert();
+				const notice:NoticeWithId = {
+					isVisible: true,
+					noticeText: 'You are log in',
+					noticeColor: 'green',
+					id: user.id,
+				};
 
-			res.json(notice);
+				res.json(notice);
+			}
+			if (user.password !== body.password) {
+				const notice:Notice = {
+					isVisible: true,
+					noticeText: 'You email or password not correct',
+					noticeColor: 'red',
+				};
+
+				res.json(notice);
+			}
 		}
 	});
